2fd9b220f294d00f6364d5423fde6476959ea06a
GARbINURCODE <strelets7953@gmail.com>
2022-11-19 01:08:45 +0300

    Translating txt and docx files

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import telebot
+from telebot import types
 import config
 import messages
 import totranslate
 
 
 bot = telebot.TeleBot(config.token)
-translation = totranslate.TranslatorCl()
+translator = totranslate.TranslatorCl()
 
 
 @bot.message_handler(commands=["start"])
-def greetings(message):
-    bot.send_message(message.chat.id, messages.hello)
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    button1 = types.KeyboardButton('Translate text')
+    button2 = types.KeyboardButton('Translate document')
+    markup.add(button1, button2)
+
+    bot.send_message(message.chat.id, messages.hello
+                     , reply_markup=markup)
 
 
-@bot.message_handler(commands=["language"])
+@bot.message_handler(commands=["translate"])
 def newdestlan(message):
     msg = bot.send_message(message.chat.id, "What is ur home language?")
     bot.register_next_step_handler(msg, setdestlan)
 
 
-@bot.message_handler(commands=["totranslate"])
-def newtext(message):
-    msg = bot.send_message(message.chat.id, "Print text for translation?")
+@bot.message_handler(content_types=["text"])
+def keyboard_handler(message):
+    if message.text == 'Translate text':
+        msg = bot.send_message(message.chat.id, "What is ur home language?")
+        bot.register_next_step_handler(msg, setdestlan)
+    elif message.text == 'Translate document':
+        msg = bot.send_message(message.chat.id, "Send me your .docx or .txt file:")
+        bot.register_next_step_handler(msg, handle_docs)
+
+@bot.message_handler(content_types=["text"])
+def setdestlan(message):
+    translator.setfromlang(message.text)
+    bot.reply_to(message, "It`s ur new home language!")
+    msg = bot.send_message(message.chat.id, "Print text for translation")
     bot.register_next_step_handler(msg, settext)
+
+
+@bot.message_handler(content_types=["text"])
+def settext(message):
+    translator.settext(message.text)
+    bot.send_message(message.chat.id, translator.Translation())
 
 
 @bot.message_handler(content_types=["text"])
@@ -33,13 +57,22 @@ def text(message):
         bot.send_message(message.chat.id, "I can do it too!: " + message.text)
 
 
-def setdestlan(message):
-    translation.setfromlang(message.text)
-    bot.reply_to(message, "It`s ur new home language!")
-
-def settext(message):
-    translation.settext(message.text)
-    bot.send_message(message.chat.id, translation.translation())
-
+@bot.message_handler(content_types=["document"])
+def handle_docs(message):
+    try:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open(file_info.file_path, 'wb') as new_file:
+                new_file.write(downloaded_file)
+        doc = ''
+        if file_info.file_path.endswith(".docx"):
+            doc = translator.Docx_Translation(file_info.file_path)
+        elif file_info.file_path.endswith(".txt"):
+            doc = translator.Txt_Transaltion(file_info.file_path)
+        file = open(doc, 'rb')
+        bot.send_document(message.chat.id, file)
+        file.close()
+    except Exception:
+        print(Exception)
 
 bot.infinity_polling()
--- totranslate.py
+++ totranslate.py
@@ -1,4 +1,6 @@
+import docx
 from googletrans import Translator
+import random
 
 translator = Translator()
 
@@ -18,9 +20,31 @@ class TranslatorCl:
     def setfromlang(self, fl):
         self.fromlang = fl
 
-    def translation(self):
+    def Translation(self):
         result = translator.translate(self.text, src=self.fromlang)
         return result.text
+
+    def Docx_Translation(self, file_path):
+        doc = docx.Document()
+        file = docx.Document(file_path)
+        paragraphs = file.paragraphs
+        for paragraph in paragraphs:
+            if paragraph.text != "":
+                translated_text = translator.translate(paragraph.text, src='russian')
+                doc.add_paragraph(translated_text.text, paragraph.style.name)
+            else:
+                doc.add_paragraph(paragraph.text, paragraph.style.name)
+        name = 'documents/' + str(random.random()) + ".docx"
+        doc.save(name)
+        return name
+
+    def Txt_Transaltion(self, file_path):
+        name = 'documents/' + str(random.random()) + ".txt"
+        with open(name, 'w') as translated_file, open(file_path, 'r') as file:
+            text = file.read()
+            translalted_text = translator.translate(text, src='russian')
+            translated_file.write(translalted_text.text)
+            return name
 
 
 
