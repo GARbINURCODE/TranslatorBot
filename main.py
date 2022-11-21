5669ef3cdd4ed0f49750e60a3013d417d99ce52a
GARbINURCODE <strelets7953@gmail.com>
2022-11-21 23:08:05 +0300

    Some comments and a bit improving naming

--- main.py
+++ main.py
@@ -3,56 +3,68 @@ import keyboards
 import config
 import messages
 import totranslate
+import time
 
-
+# Some objects of classes that we need
 bot = telebot.TeleBot(config.token)
 translator = totranslate.TranslatorCl()
-keyboard = keyboards.KeyboardCl()
 
 
+# Command start handler and the suggestion to set home language in markup
 @bot.message_handler(commands=["start"])
 def start(message):
     msg = bot.send_message(message.chat.id, messages.hello,
-                           reply_markup=keyboard.hello_markup())
+                           reply_markup=keyboards.hello_markup())
     bot.register_next_step_handler(msg, hello_markup_handler)
 
 
+# The handler of the markup u can see above
 @bot.message_handler(content_types=["text"])
 def hello_markup_handler(message):
     if message.text == 'Set home language':
         msg = bot.send_message(message.chat.id, "What is ur home language?")
         bot.register_next_step_handler(msg, set_home_lan)
 
+
+# Setting new home language
+# Suggestion to choose if we want to translate a text or a document
+# For the suggestion we use the other markup
 def set_home_lan(message):
-    translator.setfromlang(message.text)
+    translator.Set_From_Lan(message.text)
     bot.reply_to(message, "It`s ur new home language!")
     msg = bot.send_message(message.chat.id, "What do u want to translate?",
-                           reply_markup=keyboard.set_type_markup())
+                           reply_markup=keyboards.set_type_markup())
     bot.register_next_step_handler(msg, set_type_markup_handler)
 
+
+# The handler of the markup u can see above
 def set_type_markup_handler(message):
     if message.text == 'Just text':
         msg = bot.send_message(message.chat.id, 'Please, print the text in your language:')
-        bot.register_next_step_handler(msg, set_text)
+        bot.register_next_step_handler(msg, text_handler)
     else:
         msg = bot.send_message(message.chat.id, 'Please, send you docx or txt file:')
         bot.register_next_step_handler(msg, docs_handler)
 
+
+# We use TranslatorCl object to translate this text
 @bot.message_handler(content_types=["text"])
-def set_text(message):
-    translator.settext(message.text)
-    msg = bot.send_message(message.chat.id, translator.Translation(),
-                           reply_markup=keyboard.hello_markup())
+def text_handler(message):
+    msg = bot.send_message(message.chat.id, translator.Translation(message.text),
+                           reply_markup=keyboards.hello_markup())
     bot.register_next_step_handler(msg, hello_markup_handler)
 
 
+# We use TranslatorCl object to translate .docx and .txt files
+# An exception because translating of file can make some problems
 @bot.message_handler(content_types=["document"])
 def docs_handler(message):
     try:
         file_info = bot.get_file(message.document.file_id)
         downloaded_file = bot.download_file(file_info.file_path)
+        bot.send_message(message.chat.id, "Please, wait a sec...")
         with open(file_info.file_path, 'wb') as new_file:
-                new_file.write(downloaded_file)
+            new_file.write(downloaded_file)
         doc = ''
         if file_info.file_path.endswith(".docx"):
             doc = translator.Docx_Translation(file_info.file_path)
@@ -60,10 +72,17 @@ def docs_handler(message):
             doc = translator.Txt_Transaltion(file_info.file_path)
         file = open(doc, 'rb')
         msg = bot.send_document(message.chat.id, file,
-                                reply_markup=keyboard.hello_markup())
+                                reply_markup=keyboards.hello_markup())
         file.close()
         bot.register_next_step_handler(msg, hello_markup_handler)
-    except Exception:
-        print(Exception)
+    except OSError:
+        print("Docs_Handler Error - Try again after 5 secs")
+        time.sleep(5)
+        msg = bot.send_message(message.chat.id, "Ooops, something gone wrong. "
+                                                "Try to send me another file.",
+                               reply_markup=keyboards.set_type_markup())
+        bot.register_next_step_handler(msg, set_type_markup_handler)
 
+
+# A stuff for bot working
 bot.infinity_polling()
--- totranslate.py
+++ totranslate.py
@@ -3,32 +3,37 @@ from googletrans import Translator
 
 translator = Translator()
 
+
+# Class Translator here
 class TranslatorCl:
-    text = ""
     fromlang = ""
     tolang = ''
 
+# The Constructor here
     def __init__(self):
-        self.text = ""
         self.fromlang = ""
         self.tolang = ""
 
-    def settext(self, t):
-        self.text = t
-
-    def setfromlang(self, fl):
+# Setting home language
+    def Set_From_Lan(self, fl):
         self.fromlang = fl
 
-    def Translation(self):
-        result = translator.translate(self.text, src=self.fromlang)
+# Translation of a text
+    def Translation(self, text):
+        result = translator.translate(text, src=self.fromlang)
         return result.text
 
+# The method translates .docx files
+# We make a new .docx file and open the one we want to translate
+# Read each paragraph in translating file and write translated text in new one
+# We should copy a paragraph style too!!!
+# Save new file and return its path
     def Docx_Translation(self, file_path):
         doc = docx.Document()
         file = docx.Document(file_path)
         paragraphs = file.paragraphs
         for paragraph in paragraphs:
-            if paragraph.text != "":
+            if paragraph.text is not None:
                 translated_text = translator.translate(paragraph.text, src=self.fromlang)
                 doc.add_paragraph(translated_text.text, paragraph.style.name)
             else:
@@ -37,6 +42,9 @@ class TranslatorCl:
         doc.save(name)
         return name
 
+# Translates .txt files
+# Just read translating file, translate its text, write translated text in new file
+# Save translated file and return its path
     def Txt_Transaltion(self, file_path):
         name = 'documents/Translated_text.txt'
         with open(name, 'w') as translated_file, open(file_path, 'r') as file:
@@ -44,7 +52,3 @@ class TranslatorCl:
             translated_text = translator.translate(text, src=self.fromlang)
             translated_file.write(translated_text.text)
             return name
-
-
-
-
